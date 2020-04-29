0a1ab3bdabbe0feacce9f2db3cdf76ec2a6ab4b6
Mateusz Lis <lismateusz1995@gmail.com>
2020-04-29 19:39:57 +0200

    minor fix

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -36,7 +36,7 @@ namespace WishList.Controllers
         }
 
         [HttpDelete]
-        public IActionResult Delete(int Id)
+        public IActionResult Delete(int id)
         {
             var item = _context.Items.FirstOrDefault(e => e.Id == id);
             _context.Items.Remove(item);
